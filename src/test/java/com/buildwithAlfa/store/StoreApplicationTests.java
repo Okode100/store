72629094d6c939e6bef7cf5c9ddd431f69d78732
Okode100 <alfakings73@gmail.com>
2025-09-24 23:23:45 +0300

    initial push update

---
+++ src/test/java/com/buildwithAlfa/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildwithAlfa.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
