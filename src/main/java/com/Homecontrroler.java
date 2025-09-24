2b4d630e0964b2f2ade302447826c8d3aed0610a
Okode100 <alfakings73@gmail.com>
2025-09-24 23:31:03 +0300

    update: BUild resource static view page

--- src/main/java/com/Homecontrroler.java
+++ src/main/java/com/Homecontrroler.java
@@ -1,8 +1,14 @@
 package com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Homecontrroler {
+    @RequestMapping("/")
+    public String index(){
+        return "index.html";
+
+    }
     
 }
